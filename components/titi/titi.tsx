bdcdb89917e11e764ff11ff9bf669c20c93f4727
COMBE Antoine <combeantoine78@gmail.com>
2024-01-02 14:59:27 +0100

    titi & toto

---
+++ components/titi/titi.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './titi.module.css'
+
+const Titi = () => (
+        <div className={styles.titi}>titi</div>
+)
+
+export default Titi;
---
+++ components/toto/toto.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './toto.module.css'
+
+const Toto = () => (
+        <div className={styles.toto}>toto</div>
+)
+
+export default Toto;
