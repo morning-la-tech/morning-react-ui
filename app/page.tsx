9c76ba90646352b4f270fa79d820f44c4ce4321d
dellamowica <clement.d@bureauxapartager.com>
2024-02-07 15:13:26 +0100

    add next auth POC

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,28 @@
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import {Titi, Toto} from "@/components";
 
-export default function Home() {
+export default async function Home() {
+    const session = await getServerSession(authOptions);
+
   return (
     <div style={{display: "flex", flexDirection: "row"}}>
-      <Toto/>
-      <Titi/>
+        {session && (
+            <div>
+                <p>Signed in as {session.user && session.user.name}</p>
+                <a href="/api/auth/signout">Sign out by link</a>
+            </div>
+        )}
+
+        {!session && (
+            <div>
+                <p>Not signed in</p>
+                <a href="/api/auth/signin">Sign In</a>
+            </div>
+        )}
+
+        <Toto/>
+        <Titi/>
     </div>
   )
 }
