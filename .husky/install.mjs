18e962f10cf81c3a4ff1c477a323bdd0e296b398
COMBE Antoine <combeantoine78@gmail.com>
2024-02-19 15:18:24 +0100

    test

--- .husky/install.mjs
+++ .husky/install.mjs
@@ -1,5 +1,5 @@
 // Skip Husky install in production
-if (process.env.NODE_ENV === 'production' || process.env.CI) {
+if (process.env.NODE_ENV === 'production') {
   process.exit(0);
 }
 const husky = (await import('husky')).default;
