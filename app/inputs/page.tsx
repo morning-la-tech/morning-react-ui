c41ef866180c625ffb23f869f88d1b326f637612
Louise Leroux <84483070+LouiseLeroux1992@users.noreply.github.com>
2024-03-07 15:23:05 +0100

    App 1429 input number (#15)
    add number input component (based on the bottindmin similar numberInput
    component)

--- app/inputs/page.tsx
+++ app/inputs/page.tsx
@@ -1,19 +1,34 @@
 'use client';
 
 import Link from 'next/link';
+import { ChangeEvent, ChangeEventHandler, useState } from 'react';
 import InputText from '@/components/inputs/InputText';
 import Container from '@/components/layout/Container';
 import Columns from '@/components/layout/Columns';
 import Column from '@/components/layout/Column';
 import { Size } from '@/util/Enum';
+import InputNumber from '@/components/inputs/inputNumber';
 
-export default function page() {
-  const renderInputs = (props: {
+export default function Page() {
+  const [value, setValue] = useState<string>('');
+  const [numberValue, setNumberValue] = useState<number | undefined>(undefined);
+
+  const handleTextChange: ChangeEventHandler<HTMLInputElement> = (e: ChangeEvent<HTMLInputElement>): void => {
+    const newValue: string = e.target.value;
+    setValue(newValue);
+  };
+
+  const handleNumberChange = (newNumberValue: number) => {
+    setNumberValue(newNumberValue);
+  };
+
+  const renderTextInputs = (props: {
     label?: string;
     isLabelBold?: boolean;
     sublabel?: string;
     size?: Size;
-    value?: string;
+    value: string;
+    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
     placeholder?: string;
     isError?: boolean;
     isDisabled?: boolean;
@@ -31,6 +46,29 @@ export default function page() {
     );
   };
 
+  const renderNumberInputs = (props: {
+    label?: string;
+    isLabelBold?: boolean;
+    sublabel?: string;
+    size?: Size;
+    value: number | undefined;
+    onChange: (value: number) => void;
+    min?: number;
+    max?: number;
+    isError?: boolean;
+    isDisabled?: boolean;
+  }) => {
+    return (
+      <>
+        <InputNumber {...props} size={Size.xs} />
+        <InputNumber {...props} size={Size.s} />
+        <InputNumber {...props} size={Size.m} />
+        <InputNumber {...props} size={Size.l} />
+        <InputNumber {...props} size={Size.xl} />
+      </>
+    );
+  };
+
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
       <Container>
@@ -38,27 +76,57 @@ export default function page() {
         <Columns>
           <Column>
             <h1>ParentInputs</h1>
-            {renderInputs({ label: 'Label', placeholder: 'With label' })}
-            {renderInputs({ label: 'Label', isLabelBold: true, placeholder: 'With bold label' })}
-            {renderInputs({ label: 'Label', sublabel: 'Sublabel', placeholder: 'With label and sublabel' })}
+            {renderTextInputs({ label: 'Label', placeholder: 'With label', value: value, onChange: handleTextChange })}
+            {renderTextInputs({
+              label: 'Label',
+              isLabelBold: true,
+              placeholder: 'With bold label',
+              value: value,
+              onChange: handleTextChange,
+            })}
+            {renderTextInputs({
+              label: 'Label',
+              sublabel: 'Sublabel',
+              placeholder: 'With label and sublabel',
+              value: value,
+              onChange: handleTextChange,
+            })}
           </Column>
           <Column>
             <h1>Inputs text</h1>
-            {renderInputs({ placeholder: 'Simple input' })}
-            {renderInputs({ placeholder: 'Error input', isError: true })}
-            {renderInputs({ placeholder: 'Disabled input', isDisabled: true })}
+            {renderTextInputs({ placeholder: 'Simple input', value: value, onChange: handleTextChange })}
+            {renderTextInputs({ placeholder: 'Error input', isError: true, value: value, onChange: handleTextChange })}
+            {renderTextInputs({
+              placeholder: 'Disabled input',
+              isDisabled: true,
+              value: value,
+              onChange: handleTextChange,
+            })}
           </Column>
           <Column>
             <h1>Inputs text with images</h1>
-            {renderInputs({
+            {renderTextInputs({
               placeholder: 'Input with image',
               imageSrc: 'https://cdn.morning.fr/logos/logo_google.png',
               imageAlt: 'google logo',
+              value: value,
+              onChange: handleTextChange,
             })}
-            {renderInputs({
-              placeholder: 'Input with image',
-              imageSrc: '/images/magnifying-glass.svg',
-              imageAlt: 'magnifying glass',
+          </Column>
+          <Column>
+            <h1>Inputs number</h1>
+            {renderNumberInputs({
+              label: 'Simple',
+              sublabel: 'Sublabel',
+              value: numberValue,
+              onChange: handleNumberChange,
+            })}
+            {renderNumberInputs({ label: 'Error', isError: true, value: numberValue, onChange: handleNumberChange })}
+            {renderNumberInputs({
+              label: 'Disabled',
+              isDisabled: true,
+              value: numberValue,
+              onChange: handleNumberChange,
             })}
           </Column>
         </Columns>
---
+++ components/inputs/inputNumber.tsx
@@ -0,0 +1,91 @@
+import React, { ChangeEvent, forwardRef, useEffect, useState } from 'react';
+import classNames from 'classnames';
+import { Size } from '@/util/Enum';
+import ParentInput from '@/components/inputs/ParentInput';
+import { InputProps } from '@/components/inputs/types';
+import styles from './input.module.css';
+
+type InputNumberProps = InputProps & {
+  value: number | undefined;
+  onChange: (value: number) => void;
+  min?: number;
+  max?: number;
+};
+
+const InputNumber = forwardRef<HTMLInputElement, InputNumberProps>(
+  ({ label, sublabel, isLabelBold, size = Size.m, value, onChange, min, max, isError, isDisabled }, ref) => {
+    const [inputValue, setInputValue] = useState<number | undefined>(value);
+
+    const getValidValue = (val: number | undefined): number => {
+      if (val === undefined) {
+        return NaN;
+      }
+      return isNaN(val) ? NaN : val;
+    };
+
+    const validateAndSet = (val: number) => {
+      if ((min !== undefined && val < min) || (max !== undefined && val > max)) {
+        return;
+      }
+      setInputValue(val);
+    };
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+      const newValue = event.target.value;
+      if (newValue === '') {
+        setInputValue(NaN);
+        return;
+      }
+      const parsedValue = parseInt(newValue, 10);
+      if (!isNaN(parsedValue)) {
+        validateAndSet(parsedValue);
+      }
+    };
+
+    const handleBlur = () => {
+      const validValue = getValidValue(inputValue);
+      onChange(validValue);
+    };
+
+    const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+      if (!(event.key === 'ArrowUp' || event.key === 'ArrowDown')) {
+        return;
+      }
+      event.preventDefault();
+      const testedValue = getValidValue(inputValue);
+      let newValue = isNaN(testedValue) ? 0 : testedValue;
+      event.key === 'ArrowUp' ? newValue++ : newValue--;
+      validateAndSet(newValue);
+    };
+
+    useEffect(() => {
+      setInputValue(value);
+    }, [value]);
+
+    return (
+      <ParentInput label={label} sublabel={sublabel} isLabelBold={isLabelBold} size={size}>
+        <input
+          type='number'
+          className={classNames(
+            styles.input,
+            styles[size],
+            { [styles.error]: isError },
+            { [styles.disabled]: isDisabled },
+          )}
+          onChange={handleChange}
+          onKeyDown={handleKeyDown}
+          onBlur={handleBlur}
+          ref={ref}
+          value={isNaN(getValidValue(inputValue)) ? '' : inputValue}
+          min={min}
+          max={max}
+          disabled={isDisabled}
+        />
+      </ParentInput>
+    );
+  },
+);
+
+InputNumber.displayName = 'InputNumber';
+
+export default InputNumber;
