be3878c283a53b32a62088e0ec08c0f900c14c9c
Geoffrey <geoffrey@morning.fr>
2024-03-13 20:59:29 +0100

    feat: time input

---
+++ components/inputs/textField/TimeInput.tsx
@@ -0,0 +1,150 @@
+import React, { ChangeEvent, forwardRef, useState, useEffect } from 'react';
+import classNames from 'classnames';
+import { format, setMinutes, setHours } from 'date-fns';
+import ParentInput from '@/components/inputs/ParentInput';
+import { BasicInputProps, InputProps } from '@/components/inputs/types';
+import styles from '../input.module.css';
+
+type TimeInputProps = BasicInputProps &
+  InputProps & {
+    value: Date;
+    onChange: (time: Date) => void;
+    setError: (isError: boolean) => void;
+    callback?: () => boolean;
+  };
+
+const TimeInput = forwardRef<HTMLInputElement, TimeInputProps>(
+  (
+    {
+      label,
+      sublabel,
+      isLabelBold,
+      size,
+      value,
+      isError,
+      isDisabled,
+      onChange,
+      setError,
+      callback,
+    },
+    ref,
+  ) => {
+    const [inputValue, setInputValue] = useState<string>(
+      format(value, 'HH:mm'),
+    );
+
+    const stringToTime = (time: string): [number, number] => {
+      const [hours, minutes] = time
+        .split(':')
+        .map((part) => (part.length ? parseInt(part.slice(-2)) : 0));
+      return [hours, minutes || 0];
+    };
+
+    const isStringValidAsTime = (time: string): boolean => {
+      if (
+        time.replace(/[0-9:]/g, '') > 0 ||
+        time.split(':').length > 2 ||
+        time.replace(/:/g, '').length > 4
+      ) {
+        return false;
+      }
+      const [hours, minutes] = stringToTime(time);
+      return (
+        hours >= 0 &&
+        hours < 24 &&
+        minutes >= 0 &&
+        minutes < 60 &&
+        (callback ? callback() : true)
+      );
+    };
+
+    /**
+     * Function used to reformat the input value to a readable time value
+     * Function is called on every change in input value
+     */
+    const reformatTime = (time: string, event): string => {
+      // First try to find a ':' in the input value to match a pattern like 'HH:mm'
+      if (time.match(/:/g)) {
+        // If matched, will then split the string using ':' to get an hour value and a minute value
+        const [hours, minutes] = time.split(':');
+        // Will reformat the string value using the last 2 char of each part
+        return `${hours.slice(-2)}:${minutes.length ? minutes.slice(-2) : ''}`;
+      }
+
+      // If the pattern 'HH:mm' is not matched, will try to see if a ':' was in previous value
+      if (inputValue.match(/:/g)) {
+        // If that was the case, means that the ':' was erased
+        // Will then split the previous value using ':' to get a previous hour value and previous minute value
+        // Erasing a ':' means that user tried to erased last digit of hour value, will reformat a time value accordingly
+        const [previousHours, previousMinutes] = inputValue.split(':');
+        // Reposition cursor depending on whether or not a char is  present in the hour value
+        setTimeout(() => {
+          event.target.focus();
+          event.target.setSelectionRange(
+            previousHours.slice(0, -1).length,
+            previousHours.slice(0, -1).length,
+          );
+        }, 0);
+        return (
+          previousHours.slice(0, -1) +
+          (previousMinutes.length ? `:${previousMinutes.slice(-2)}` : '')
+        );
+      }
+
+      // If nothing neither current nor previous values have ':', then proceed as normal
+      return time.length < 2 ? time : `${time}:`;
+    };
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+      const newValue = event.target.value;
+      const reformatedNewValue = reformatTime(newValue, event);
+
+      setError(!isStringValidAsTime(reformatedNewValue));
+
+      if (isStringValidAsTime(reformatedNewValue)) {
+        setInputValue(reformatedNewValue);
+      }
+    };
+
+    const handleBlur = () => {
+      const [hours, minutes] = stringToTime(inputValue);
+      setError(false);
+      onChange(setMinutes(setHours(new Date(), hours), minutes));
+    };
+
+    useEffect(() => {
+      setInputValue(format(value, 'HH:mm'));
+    }, [value]);
+
+    return (
+      <ParentInput
+        label={label}
+        sublabel={sublabel}
+        isLabelBold={isLabelBold}
+        size={size}
+      >
+        <div className={styles.wrapper}>
+          <input
+            className={classNames(
+              styles.input,
+              styles[size],
+              { [styles.error]: isError },
+              { [styles.disabled]: isDisabled },
+            )}
+            ref={ref}
+            placeholder={'HH:MM'}
+            disabled={isDisabled}
+            type='text'
+            value={inputValue}
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+        </div>
+      </ParentInput>
+    );
+  },
+);
+
+TimeInput.displayName = 'TimeInput';
+
+export default TimeInput;
