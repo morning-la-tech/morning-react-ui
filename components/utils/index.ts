a675cb85ae265741dcfdc6076a33238180642707
COMBE Antoine <combeantoine78@gmail.com>
2024-06-21 09:25:53 +0200

    Add forgotten export

--- components/utils/index.ts
+++ components/utils/index.ts
@@ -5,3 +5,5 @@ export { default as Loading } from 'morning-react-ui/components/utils/Loading';
 export { default as Separator } from 'morning-react-ui/components/utils/Separator';
 
 export { default as Toast } from 'morning-react-ui/components/utils/Toast';
+
+export { default as ToolTip } from 'morning-react-ui/components/utils/Tooltip';
