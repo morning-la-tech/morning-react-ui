09a7d958ec9e2bb08ed2dd3eb9012ad2afe12554
COMBE Antoine <combeantoine78@gmail.com>
2024-07-11 11:26:20 +0200

    split selectionStateUtils

---
+++ utils/selectionState/selectionStateModifiers.ts
@@ -0,0 +1,77 @@
+import { SelectionState } from 'morning-react-ui/types/dataTypes';
+
+const setAllTrue = (selectionState: SelectionState): SelectionState => {
+  const updatedState: SelectionState = { ...selectionState };
+  Object.keys(updatedState).forEach((key) => {
+    updatedState[key] = true;
+  });
+  return updatedState;
+};
+
+const setAllFalse = (selectionState: SelectionState): SelectionState => {
+  const updatedState: SelectionState = { ...selectionState };
+  Object.keys(updatedState).forEach((key) => {
+    updatedState[key] = false;
+  });
+  return updatedState;
+};
+
+const updateSelectionState = (
+  currentState: SelectionState,
+  keyToUpdate: string,
+  newValue: boolean,
+): SelectionState => {
+  const updatedState: SelectionState = { ...currentState };
+  updatedState[keyToUpdate] = newValue;
+  return updatedState;
+};
+
+const setAtTrueAndOthersAtFalse = (
+  selectionState: SelectionState,
+  keyToUpdate: string,
+): SelectionState => {
+  const updatedState: SelectionState = { ...selectionState };
+  Object.keys(updatedState).forEach((key) => {
+    updatedState[key] = false;
+  });
+
+  updatedState[keyToUpdate] = true;
+  return updatedState;
+};
+
+const removeLastElement = (
+  selectionStateToRemove: SelectionState,
+): SelectionState => {
+  const keys = Object.keys(selectionStateToRemove);
+  const lastKey = keys[keys.length - 1];
+  if (lastKey) {
+    const { [lastKey]: _, ...remainingSelectionState } = selectionStateToRemove;
+    return remainingSelectionState;
+  }
+  return selectionStateToRemove;
+};
+
+const toggleSelectionStateAtIndex = (
+  state: SelectionState,
+  position: number | null,
+): SelectionState => {
+  const keys = Object.keys(state);
+  if (position === null || position < 0 || position >= keys.length) {
+    return state;
+  }
+
+  const keyToToggle = keys[position];
+  return {
+    ...state,
+    [keyToToggle]: !state[keyToToggle],
+  };
+};
+
+export {
+  setAllTrue,
+  setAllFalse,
+  updateSelectionState,
+  setAtTrueAndOthersAtFalse,
+  removeLastElement,
+  toggleSelectionStateAtIndex,
+};
