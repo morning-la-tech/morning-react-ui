07cb9d32054fa519a64d1978ca493e22db2f74a3
COMBE Antoine <combeantoine78@gmail.com>
2024-09-02 17:54:23 +0200

    fix cookie name

--- app/auth.ts
+++ app/auth.ts
@@ -1,7 +1,7 @@
 import NextAuth from 'next-auth';
 import Google from 'next-auth/providers/google';
 
-const useSecureCookies = `${process.env.NEXTAUTH_URL}`.startsWith('https://');
+const useSecureCookies = `${process.env.AUTH_URL}`.startsWith('https://');
 const cookiePrefix = useSecureCookies ? '__Secure-' : '';
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
