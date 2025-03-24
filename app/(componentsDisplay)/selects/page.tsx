278fdde62998632abbd972ba60f0e23196aa179a
Noé Maire-Amiot <noe@morning.fr>
2025-03-24 15:33:59 +0100

    feat: Upgrade mutli-select input data

--- app/(componentsDisplay)/selects/page.tsx
+++ app/(componentsDisplay)/selects/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+
 import { useState } from 'react';
 import MultiSelect from 'morning-react-ui/components/inputs/selects/MultiSelect';
 import SelectInput from 'morning-react-ui/components/inputs/selects/SelectInput';
@@ -7,70 +8,68 @@ import Column from 'morning-react-ui/components/layout/Column';
 import Columns from 'morning-react-ui/components/layout/Columns';
 import Container from 'morning-react-ui/components/layout/Container';
 import Navigation from 'morning-react-ui/components/layout/Navigation';
-import { SelectionState, SelectOption } from 'morning-react-ui/types';
+import { SelectOption } from 'morning-react-ui/types'; // { id, label }
 import { Size } from 'morning-react-ui/utils/Enum';
 
+const initialOptions: SelectOption[] = [
+  { id: 'amsterdam', label: 'Amsterdam' },
+  { id: 'argentine', label: 'Argentine' },
+  { id: 'auber', label: 'Auber' },
+  { id: 'balard', label: 'Balard' },
+  { id: 'bayard', label: 'Bayard' },
+  { id: 'beaurepaire', label: 'Beaurepaire' },
+  { id: 'boissiere', label: 'Boissière' },
+  { id: 'bourse', label: 'Bourse' },
+  { id: 'caire', label: 'Caire' },
+  { id: 'cambaceres', label: 'Cambacérès' },
+  { id: 'clery', label: 'Cléry' },
+  { id: 'concorde', label: 'Concorde' },
+  { id: 'faubourg_saint_honore', label: 'Faubourg Saint-Honoré' },
+  { id: 'gallieni', label: 'Gallieni' },
+  { id: 'iena', label: 'Iéna' },
+  { id: 'la_defense', label: 'La Défense' },
+  { id: 'laffitte', label: 'Laffitte' },
+  { id: 'miromesnil', label: 'Miromesnil' },
+  { id: 'mogador', label: 'Mogador' },
+  { id: 'monceau', label: 'Monceau' },
+  { id: 'montparnasse', label: 'Montparnasse' },
+  { id: 'montsouris', label: 'Montsouris' },
+  { id: 'neuilly_charles_de_gaulle', label: 'Neuilly Charles de Gaulle' },
+  { id: 'neuilly_hotel_de_ville', label: 'Neuilly Hôtel de Ville' },
+  { id: 'penthievre', label: 'Penthièvre' },
+  { id: 'pereire', label: 'Pereire' },
+  { id: 'pont_de_lalma', label: "Pont de l'Alma" },
+  { id: 'pont_de_neuilly', label: 'Pont de Neuilly' },
+  { id: 'republique_boulanger', label: 'République-Boulanger' },
+  { id: 'saint_ouen', label: 'Saint Ouen' },
+  { id: 'saint_augustin', label: 'Saint-Augustin' },
+  { id: 'stalingrad', label: 'Stalingrad' },
+  { id: 'trevise', label: 'Trévise' },
+  { id: 'trocadero', label: 'Trocadéro' },
+  { id: 'vivienne', label: 'Vivienne' },
+  { id: '54_petites_ecuries_ratp', label: '54 Petites Écuries / RATP' },
+  { id: 'cadet', label: 'Cadet' },
+  { id: 'clichy', label: 'Clichy' },
+  { id: 'duhesme', label: 'Duhesme' },
+  { id: 'dulong', label: 'Dulong' },
+  { id: 'dussoubs', label: 'Dussoubs' },
+  { id: 'ledru_rollin', label: 'Ledru-Rollin' },
+  { id: 'petites_ecuries', label: 'Petites Écuries' },
+  { id: 'sentier', label: 'Sentier' },
+  { id: 'toudic', label: 'Toudic' },
+  { id: 'victoires', label: 'Victoires' },
+];
+
 export default function Selects() {
-  const initialOptions: SelectOption[] = [
-    { id: 'amsterdam', label: 'Amsterdam' },
-    { id: 'argentine', label: 'Argentine' },
-    { id: 'auber', label: 'Auber' },
-    { id: 'balard', label: 'Balard' },
-    { id: 'bayard', label: 'Bayard' },
-    { id: 'beaurepaire', label: 'Beaurepaire' },
-    { id: 'boissiere', label: 'Boissière' },
-    { id: 'bourse', label: 'Bourse' },
-    { id: 'caire', label: 'Caire' },
-    { id: 'cambaceres', label: 'Cambacérès' },
-    { id: 'clery', label: 'Cléry' },
-    { id: 'concorde', label: 'Concorde' },
-    { id: 'faubourg_saint_honore', label: 'Faubourg Saint-Honoré' },
-    { id: 'gallieni', label: 'Gallieni' },
-    { id: 'iena', label: 'Iéna' },
-    { id: 'la_defense', label: 'La Défense' },
-    { id: 'laffitte', label: 'Laffitte' },
-    { id: 'miromesnil', label: 'Miromesnil' },
-    { id: 'mogador', label: 'Mogador' },
-    { id: 'monceau', label: 'Monceau' },
-    { id: 'montparnasse', label: 'Montparnasse' },
-    { id: 'montsouris', label: 'Montsouris' },
-    { id: 'neuilly_charles_de_gaulle', label: 'Neuilly Charles de Gaulle' },
-    { id: 'neuilly_hotel_de_ville', label: 'Neuilly Hôtel de Ville' },
-    { id: 'penthievre', label: 'Penthièvre' },
-    { id: 'pereire', label: 'Pereire' },
-    { id: 'pont_de_lalma', label: "Pont de l'Alma" },
-    { id: 'pont_de_neuilly', label: 'Pont de Neuilly' },
-    { id: 'republique_boulanger', label: 'République-Boulanger' },
-    { id: 'saint_ouen', label: 'Saint Ouen' },
-    { id: 'saint_augustin', label: 'Saint-Augustin' },
-    { id: 'stalingrad', label: 'Stalingrad' },
-    { id: 'trevise', label: 'Trévise' },
-    { id: 'trocadero', label: 'Trocadéro' },
-    { id: 'vivienne', label: 'Vivienne' },
-    { id: '54_petites_ecuries_ratp', label: '54 Petites Écuries / RATP' },
-    { id: 'cadet', label: 'Cadet' },
-    { id: 'clichy', label: 'Clichy' },
-    { id: 'duhesme', label: 'Duhesme' },
-    { id: 'dulong', label: 'Dulong' },
-    { id: 'dussoubs', label: 'Dussoubs' },
-    { id: 'ledru_rollin', label: 'Ledru-Rollin' },
-    { id: 'petites_ecuries', label: 'Petites Écuries' },
-    { id: 'sentier', label: 'Sentier' },
-    { id: 'toudic', label: 'Toudic' },
-    { id: 'victoires', label: 'Victoires' },
-  ];
+  const complexOptions = initialOptions.map((option) => ({
+    value: option.id,
+    label: option.label,
+  }));
 
-  const [options, setOptions] = useState<SelectionState>(
-    initialOptions.reduce(
-      (acc, option) => ({ ...acc, [option.label]: false }),
-      {},
-    ),
-  );
+  const [optionsToDisplay, setOptionsToDisplay] = useState<number | null>(8);
 
-  const [optionsToDisplay, setOptionsToDisplay] = useState<
-    number | null | undefined
-  >(8);
   const [selectValue, setSelectValue] = useState<SelectOption | null>(null);
+  const [multiValues, setMultiValues] = useState<string[]>([]);
 
   const handleSelectChange = (newSelectValue: SelectOption) => {
     setSelectValue(newSelectValue);
@@ -103,12 +102,13 @@ export default function Selects() {
     sublabel?: string;
     bold?: boolean;
     size?: Size;
-    options: SelectionState;
+    options: { value: string; label: string }[];
     placeholder?: string;
     disabled?: boolean;
     isError?: boolean;
-    onChange: (value: SelectionState) => void;
     rowToDisplay?: number | null;
+    onChange: (values: string[]) => void;
+    values: string[];
   }) => {
     const sentProps = { ...props, rowToDisplay: props?.rowToDisplay || 1 };
     return (
@@ -132,6 +132,7 @@ export default function Selects() {
           value={optionsToDisplay}
           onChange={setOptionsToDisplay}
         />
+
         <Columns>
           <Column>
             <h1>MultiSelect</h1>
@@ -139,29 +140,33 @@ export default function Selects() {
               label: 'Simple',
               sublabel: 'Sublabel',
               placeholder: 'Placeholder',
-              options: options,
-              onChange: setOptions,
+              options: complexOptions,
+              values: multiValues,
+              onChange: setMultiValues,
               rowToDisplay: optionsToDisplay,
             })}
             {renderMultiSelectInputs({
-              label: 'Simple',
+              label: 'Simple (disabled)',
               sublabel: 'Sublabel',
               placeholder: 'Placeholder',
-              options: options,
-              onChange: setOptions,
+              options: complexOptions,
+              values: multiValues,
+              onChange: setMultiValues,
               rowToDisplay: optionsToDisplay,
               disabled: true,
             })}
             {renderMultiSelectInputs({
-              label: 'Simple',
+              label: 'Simple (error)',
               sublabel: 'Sublabel',
               placeholder: 'Placeholder',
-              options: options,
-              onChange: setOptions,
+              options: complexOptions,
+              values: multiValues,
+              onChange: setMultiValues,
               rowToDisplay: optionsToDisplay,
               isError: true,
             })}
           </Column>
+
           <Column>
             <h1>SelectInput</h1>
             {renderSelectInputs({
