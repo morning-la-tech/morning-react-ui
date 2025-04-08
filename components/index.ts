f2499bd4cbd77cff0f9c244152ad6b9b24bca861
Noé Maire-Amiot <noe@morning.fr>
2025-04-08 15:16:58 +0200

    fix: add index to table and tag

--- components/index.ts
+++ components/index.ts
@@ -9,3 +9,7 @@ export * from 'morning-react-ui/components/upload/index';
 export * from 'morning-react-ui/components/utils/index';
 
 export * from 'morning-react-ui/components/inputs/wrapped/index';
+
+export * from 'morning-react-ui/components/table/index';
+
+export * from 'morning-react-ui/components/tag/index';
